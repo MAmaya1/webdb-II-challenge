8cfb6c0f44086ab6f7f2567747589b65f9b133e1
Mario Amaya <thefozcat@gmail.com>
2019-04-30 14:33:19 -0700

    added DELETE to zoos-router

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -83,4 +83,22 @@ router.put('/:id', (req, res) => {
         })
 })
 
+// DELETE zoo
+
+router.delete('/:id', (req, res) => {
+    db('zoos')
+        .where({ id: req.params.id })
+        .del(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: `${count} ${count > 1 ? 'records' : 'record'} deleted.` })
+            } else {
+                res.status(404).json({ errorMessage: 'A zoo with the specified ID does not exist.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'Zoo could not be deleted from the database.' })
+        })
+})
+
 module.exports = router;
