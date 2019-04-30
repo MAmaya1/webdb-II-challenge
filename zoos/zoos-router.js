85d80a3bce0baa8e3e8c5f36fc06f07a1f1d3d60
Mario Amaya <thefozcat@gmail.com>
2019-04-30 13:15:58 -0700

    added GET to zoos-router

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -2,4 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
+// Import Knex
+
+const knex = require('knex');
+
+// Configure Knex
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.sqlite3'
+    },
+    useNullAsDefault: true
+}
+
+const db = knex(knexConfig);
+
+// GET zoos
+
+router.get('/', (req, res) => {
+    db('zoos')
+        .then(zoos => {
+            res.status(201).json(zoos)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'Data could not be retrieved.' })
+        })
+})
+
 module.exports = router;
